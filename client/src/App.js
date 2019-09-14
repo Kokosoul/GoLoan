8abe59d846468f520c0f3da74b32f887e73fddc2
Yoshi <ghowell1228@gmail.com>
2019-09-14 19:41:41 -0400

    Implemented React front end

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          GoLoan.ga is currently in development.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn More
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
