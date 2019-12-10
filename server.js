b32c84b77647e3dd62904af5111eb3281f48be85
Yoshi <ghowell1228@gmail.com>
2019-12-10 01:17:42 -0500

    for deploy

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
-const express = require('express');
-const connectDB = require('./config/db')
-
+const express = require("express");
+const connectDB = require("./config/db");
+const path = requrie("path");
 const app = express();
 
 // Connect Database
 connectDB();
 
 // Init Middleware
-app.use(express.json({ extended: false }))
-
-app.get('/', (req, res) => res.send('API Running'));
+app.use(express.json({ extended: false }));
 
 // Define Routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
+// Serve Static Assets in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5800;
 
